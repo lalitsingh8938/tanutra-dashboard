f42120ef8e924f06d76cd7a4362bd097fc28c5c5
Your Name <your.email@example.com>
2025-02-19 18:41:35 +0530

    editvendorprofile changes

--- src/components/EditVendorProfile.jsx
+++ src/components/EditVendorProfile.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect,axios } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
